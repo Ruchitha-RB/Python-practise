2fb477b7b09e87d5908e9a5759f56315febd7dca
Ruchitha-RB <Ruchitha14111998@gmail.com>
2025-02-10 17:37:41 +0530

    minor changes

--- API.py
+++ API.py
@@ -7,23 +7,21 @@ data = {
     "salary": 4500000.00,
     "status": False
 }
-# Dump the dictionary into a JSON file
+# Dump the python dictionary into a JSON file
 with open("data.json", "w") as file:
-    json.dump(data, file, indent=4)  # `indent=4` makes it more readable
+    json.dump(data, file, indent=4)
 
-#CReate a string python dictionary
-Employee ={"name":"Ruchitha","ID":"TGTS106","Role":"Software Engineer"}
-#Dump the dictionary into json string
+#Creating python dictionary and then dump into a JSON file
+Employee ={"name":"Ruchitha.Boddu","ID":"TGTS106","Role":"Software Engineer"}
 with open("Employee.json","w") as file:
     json.dump(Employee,file,indent=1)
 
-    data = {"id": 106, "name": "Ruchi", "salary": 5555555.00, "status": True}
 
+#Creating python dictionary, and then dump into a  JSON String
+    data = {"id": 106, "name": "Ruchi", "salary": 5555555.00, "status": True}
     json_string = json.dumps(data,indent=2)  # Convert to JSON string
     print(json_string)  # Check JSON format
-
-    # Verify the type
-    print("Data Type:", type(json_string))  # Should print: <class 'str'
+    print("Data Type:", type(json_string))
 
 
 
