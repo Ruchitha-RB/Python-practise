133b4dc90977f1d44e681dc77feed30411eb1c81
Ruchitha-RB <Ruchitha14111998@gmail.com>
2025-02-13 19:05:37 +0530

    Serialization and De-serialization

--- API.py
+++ API.py
@@ -10,7 +10,7 @@ data = {
 with open("my_data.json", "w") as file:  #  Open file named(my_data.json) to Write JSON data
     json.dump(data, file, indent=4)   # Convert dictionary to JSON and write to file as text
 with open("my_data.json","r") as file:
-    idata=json.load(file)
+    idata=json.load(file) #de-serialization
     print("Ex1=Data Type:",type(idata),) # Should print <class 'dict'>
     print("JSON file to Python Object:",idata)
 
@@ -19,7 +19,7 @@ Employee = {"name": "Ruchitha.Boddu", "ID": "TGTS106", "Role": "Software Enginee
 with open("Member.json", "w") as file:  # open file named(Member.json) to write JSON data
     json.dump(Employee, file, indent=4)  # Convert python dictionary to JSON and write to file
 with open("Member.json", "r") as file:  # Read JSON from a file and convert to Python object
-     pfile=json.load(file) ## Convert JSON to dictionary
+     pfile=json.load(file) ## Convert JSON to dictionary, de-serialization
      print("Ex2=Data Type:", type(pfile))  # Should print <Class 'dict'>
      print("JSON file to python Object:",pfile)
 
@@ -29,7 +29,7 @@ json_string = json.dumps(dummy, indent=2) # Convert dictionary to JSON string
 print(json_string)  # Check JSON format
 print("Ex3=Data Type:", type(json_string))  # Should be <class 'str'> only for READ
 with open("dummy.json","r") as file:
-    dfile=json.loads(json_string)
+    dfile=json.loads(json_string) # de-serialization
     print("JSON String to Python Object:",dfile)
 
 
