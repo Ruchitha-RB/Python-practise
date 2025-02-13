60c527f4741f1f9bd04dd4a43d0a1f4748849288
Ruchitha-RB <Ruchitha14111998@gmail.com>
2025-02-13 17:05:19 +0530

    Thrusday updated

--- API.py
+++ API.py
@@ -5,25 +5,25 @@ print(json.__file__)  # Checking the location of the JSON module
 data = {
     "id": 101,
     "name": "Ruchi",
-    "salary": 4500000.00,
-    "status": False
+    "salary": 4500000,
 }
-# Dump the Python dictionary into a JSON file
-with open("data.json", "w") as file:  # Write into JSON file
-    json.dump(data, file, indent=4)
+with open("my_data.json", "w") as file:  #  Open file named(my_data.json) to Write JSON data
+    json.dump(data, file, indent=4)   # Convert dictionary to JSON and write to file
+    print("Ex1=Data Type:",type(data))
 
 # Example 2: Creating a Python dictionary and dumping it into a JSON file
-Employee = {"name": "Ruchitha.Boddu", "ID": "TGTS106", "Role": "Software Engineer"}
-with open("Employee.json", "w") as file:
-    json.dump(Employee, file, indent=4)
+Employee = {"name": "Ruchitha.Boddu", "ID": "TGTS106", "Role": "Software Engineer"} # Creating the dictionary
+with open("Member.json", "w") as file:  # open file named(Member.json) to write JSON data
+    json.dump(Employee, file, indent=4)  # Convert python dictionary to JSON and write to file
+    print("Ex2=Data Type:",type(Employee)) # Should print <Class ''> CRUD operations
 
 # Example 3: Creating a Python dictionary and converting it into a JSON string
-dummy = {"id": 106, "name": "Ruchi", "salary": 5555555.00}
-# Convert dictionary to JSON string and print it
-json_string = json.dumps(dummy, indent=2)
+dummy = {"id": 106, "name": "Ruchi", "salary": 5555555}
+json_string = json.dumps(dummy, indent=2) # Convert dictionary to JSON string
 print(json_string)  # Check JSON format
-print("Data Type:", type(json_string))  # Should be <class 'str'>
+print("Ex3=Data Type:", type(json_string))  # Should be <class 'str'> only for READ
 
-# Writing JSON string to a file properly
-with open("dummy.json", "w") as file:
-    file.write(json_string)  # Save JSON string into file
+# Writing JSON string back to a file
+with open("dummy.json", "w") as file: # # Open/create "dummy.json" in write mode ("w")
+     file.write(json_string)  #  writes the JSON string to the file as text.
+     print("Ex3=Data Type:",type(dummy))
